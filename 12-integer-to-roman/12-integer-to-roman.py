9facc7bf071c33fe27a5f77d2bb1d2d409f093e2
choesuhong <57895652+choesuhong@users.noreply.github.com>
2022-06-24 18:46:22 +0900

    Time: 71 ms (57.20%), Space: 13.9 MB (79.72%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,30 +1,8 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        i_to_Roman = {
-            1: 'I',
-            4: 'IV',
-            5: 'V',
-            9: 'IX',
-            10: 'X',
-            40: 'XL',
-            50: 'L',
-            90: 'XC',
-            100: 'C',
-            400: 'CD',
-            500: 'D',
-            900: 'CM',
-            1000: 'M'
-        }
+        M = ['','M','MM','MMM']
+        C = ['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM']
+        X = ['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC']
+        I = ['','I','II','III','IV','V','VI','VII','VIII','IX']
         
-        i_to_Roman = sorted(i_to_Roman.items(), key= lambda item: -item[0])
-        
-        res = ''
-        
-        while num>0:
-            for i, R in i_to_Roman:
-                if num>=i:
-                    res += R
-                    num-=i
-                    break
-        
-        return res
+        return M[num//1000] + C[num%1000//100] + X[num%100//10] + I[num%10]
