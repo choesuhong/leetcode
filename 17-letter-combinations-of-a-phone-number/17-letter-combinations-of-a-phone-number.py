250269e7a61c880f325072faabe04bbf4931d70d
choesuhong <57895652+choesuhong@users.noreply.github.com>
2022-06-27 15:19:36 +0900

    Time: 58 ms (16.89%), Space: 13.8 MB (77.82%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,28 @@
+from itertools import permutations
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        digits_dic = {
+            '2':['a','b','c'],
+            '3':['d','e','f'],
+            '4':['g','h','i'],
+            '5':['j','k','l'],
+            '6':['m','n','o'],
+            '7':['p','q','r','s'],
+            '8':['t','u','v'],
+            '9':['w','x','y','z']
+        }
+        if not digits:
+            return []
+        
+        res = digits_dic[digits[0]]
+        
+        for i in range(1,len(digits)):
+            new_res = []
+            for s1 in res:
+                for s2 in digits_dic[digits[i]]:
+                    new_res.append(s1+s2)
+            res = new_res
+        
+        return res
+        
+        # print(list(permutations(map(lambda x: digits_dic[x], digits))))
