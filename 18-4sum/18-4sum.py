c90a3ca185d42bbc8a0167e01f63d1ce0847adfe
choesuhong <57895652+choesuhong@users.noreply.github.com>
2022-07-12 19:28:30 +0900

    Time: 154 ms (87.61%), Space: 13.9 MB (93.72%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -1,22 +1,55 @@
 class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
+    # def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+#         nums.sort()
         
-        res = []
+#         res = []
         
-        for i in range(len(nums)-3):
-            for j in range(i+1,len(nums)-2):
-                l = j+1
-                r = len(nums)-1
-                while l<r:
-                    while l<r and nums[i]+nums[j]+nums[l]+nums[r]<target:
-                        l+=1
-                    while l<r and nums[i]+nums[j]+nums[l]+nums[r]>target:
-                        r-=1
-                    if l<r and nums[i]+nums[j]+nums[l]+nums[r]==target:
-                        if not [nums[i],nums[j],nums[l],nums[r]] in res:
-                            res.append([nums[i],nums[j],nums[l],nums[r]])
-                        l+=1
-                        r-=1
+#         for i in range(len(nums)-3):
+#             for j in range(i+1,len(nums)-2):
+#                 l = j+1
+#                 r = len(nums)-1
+#                 while l<r:
+#                     while l<r and nums[i]+nums[j]+nums[l]+nums[r]<target:
+#                         l+=1
+#                     while l<r and nums[i]+nums[j]+nums[l]+nums[r]>target:
+#                         r-=1
+#                     if l<r and nums[i]+nums[j]+nums[l]+nums[r]==target:
+#                         if not [nums[i],nums[j],nums[l],nums[r]] in res:
+#                             res.append([nums[i],nums[j],nums[l],nums[r]])
+#                         l+=1
+#                         r-=1
                 
-        return res
+#         return res
+
+    def fourSum(self, nums, target):
+        nums.sort()
+        results = []
+        self.findNsum(nums, target, 4, [], results)
+        return results
+
+    def findNsum(self, nums, target, N, result, results):
+        if len(nums) < N or N < 2: return
+
+        # solve 2-sum
+        if N == 2:
+            l,r = 0,len(nums)-1
+            while l < r:
+                if nums[l] + nums[r] == target:
+                    results.append(result + [nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                    while r > l and nums[r] == nums[r + 1]:
+                        r -= 1
+                elif nums[l] + nums[r] < target:
+                    l += 1
+                else:
+                    r -= 1
+        else:
+            for i in range(0, len(nums)-N+1):   # careful about range
+                if target < nums[i]*N or target > nums[-1]*N:  # take advantages of sorted list
+                    break
+                if i == 0 or i > 0 and nums[i-1] != nums[i]:  # recursively reduce N
+                    self.findNsum(nums[i+1:], target-nums[i], N-1, result+[nums[i]], results)
+        return
