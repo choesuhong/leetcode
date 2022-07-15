2a279c1c003febeaaec62e797f0bfaad2aba192c
choesuhong <57895652+choesuhong@users.noreply.github.com>
2022-07-15 18:32:41 +0900

    Time: 58 ms (42.91%), Space: 14.1 MB (97.92%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,23 +1,16 @@
-from collections import Counter
 class Solution:
-    def plus_parentheses(self, s: str, n: int):
-        C = Counter(s)
-        new_s = []
-        if C['('] > C[')']:
-            new_s.append(s + ')')
-        if C['('] < n:
-            new_s.append(s + '(')
-        return new_s
-    
+    def generate(self, left, right, res, s, n):
+        if len(s) == n*2:
+            res.append(s)
+            return
+        
+        if left < n:
+            self.generate(left+1, right, res, s+'(', n)
+        if right < left:
+            self.generate(left, right+1, res, s+')', n)
+        
     def generateParenthesis(self, n: int) -> List[str]:
-        res = ['(']
+        res = []
+        self.generate(0,0,res,'',n)
         
-        for i in range(n*2-1):
-            tmp = []
-            for r in res:
-                tmp += self.plus_parentheses(r, n)
-            res = tmp
-            
         return res
-        
-        
