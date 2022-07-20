91efd22d61dda0a2534f518434860c976cef477e
choesuhong <57895652+choesuhong@users.noreply.github.com>
2022-07-20 22:26:02 +0900

    Time: 68 ms (64.15%), Space: 15.2 MB (85.07%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
@@ -4,27 +4,43 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dummy = prev = ListNode()
-        dummy.next = cur = head
-        while True:
-            cnt = 0
+#     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+#         dummy = prev = ListNode()
+#         dummy.next = cur = head
+#         while True:
+#             cnt = 0
             
-            s = []
-            while cur and cnt<k:
-                cnt+=1
-                s.append(cur)
-                cur = cur.next
+#             s = []
+#             while cur and cnt<k:
+#                 cnt+=1
+#                 s.append(cur)
+#                 cur = cur.next
             
-            if cnt == k:
-                for _ in range(k):
-                    prev.next = s.pop()
-                    prev = prev.next
+#             if cnt == k:
+#                 for _ in range(k):
+#                     prev.next = s.pop()
+#                     prev = prev.next
             
-            else:
-                prev.next = s[0] if s else None
-                break
+#             else:
+#                 prev.next = s[0] if s else None
+#                 break
 
-            prev.next = cur
+#             prev.next = cur
                 
-        return dummy.next
+#         return dummy.next
+    def reverseKGroup(self, head, k):
+        dummy = jump = ListNode(0)
+        dummy.next = l = r = head
+
+        while True:
+            count = 0
+            while r and count < k:   # use r to locate the range
+                r = r.next
+                count += 1
+            if count == k:  # if size k satisfied, reverse the inner linked list
+                pre, cur = r, l
+                for _ in range(k):
+                    cur.next, cur, pre = pre, cur.next, cur  # standard reversing
+                jump.next, jump, l = pre, l, r  # connect two k-groups
+            else:
+                return dummy.next
